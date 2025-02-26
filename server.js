2ff2f0abe42c1bc5ba4552ffadff98891696bbaf
Tomas Maldocena <104042642+tmaldocena@users.noreply.github.com>
2025-02-26 16:38:25 -0300

    added dotenv

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require("express");
 const webPush = require("web-push");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const dotenv = require('dotenv');
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+dotenv.config();
 
 // Claves VAPID (Generarlas con web-push)
-const publicVapidKey = "BEsp2Ohtz7PUul7D8s0LpwsG1HflEt6CxqgBOl8kz-NXOwPTA53p1UG2JzPmEKGCvsd7lGjEykR-hH50piOdEv4";
-const privateVapidKey = "nXc9M_54FerqtFMoo1_eN_GJOPFa8xqEjyHVqpqoJmA";
+const publicVapidKey = process.env.VAPID_PUBLIC_KEY || "BEsp2Ohtz7PUul7D8s0LpwsG1HflEt6CxqgBOl8kz-NXOwPTA53p1UG2JzPmEKGCvsd7lGjEykR-hH50piOdEv4";
+const privateVapidKey = process.env.VAPID_PRIVATE_KEY || "nXc9M_54FerqtFMoo1_eN_GJOPFa8xqEjyHVqpqoJmA";
 
 webPush.setVapidDetails("mailto:tomasmaldocena@gmail.com", publicVapidKey, privateVapidKey);
 
